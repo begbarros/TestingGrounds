4313f46209c65392625f567793e054c43f81a903
Bruno E G Barros <begbarros@gmail.com>
2017-07-21 09:24:40 -0300

    Gate Opacity bug fixed, Spherecast with spawn trace channel created, ground, gate and sensors ignores spawn channel

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -2,6 +2,7 @@
 
 #include "Tile.h"
 #include "Engine/World.h"
+#include  "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -17,6 +18,23 @@ ATile::ATile()
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
+	SphereCast(GetActorLocation(), 500);
+}
+
+bool ATile::SphereCast(FVector Location, float Radius)
+{
+	FHitResult Hit;
+	bool bHitSomething = GetWorld()->SweepSingleByChannel(
+		Hit,
+		Location,
+		Location,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2, // Spawn Channel defined in /Config/DefaultEngine.ini 
+		FCollisionShape::MakeSphere(Radius)
+	);
+	bHitSomething ? DrawDebugSphere(GetWorld(), Location, Radius, 12, FColor::Red, true) : DrawDebugSphere(GetWorld(), Location, Radius, 12, FColor::Green, true); 
+	
+	return bHitSomething;
 }
 
 void ATile::PlaceActor( TSubclassOf<AActor> ToBeSpawned, int32 min, int32 max)
--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -22,6 +22,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	bool SphereCast(FVector Location, float Radius);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
